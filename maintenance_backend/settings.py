3320aff75505f52bb65ab37bac894fe7febe7e23
scottessner <scott.essner@gmail.com>
2017-03-11 15:14:24 -0600

    fixing settings and fab

--- maintenance_backend/settings.py
+++ maintenance_backend/settings.py
@@ -123,16 +123,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
-CORS_ORIGIN_WHITELIST = [
-    '127.0.0.1:4200',
-    'localhost:4200'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'api.maintenance.ssessner.com',
+#     'localhost:4200'
+# ]
 
-from corsheaders.defaults import default_headers
-CORS_ALLOW_HEADERS = default_headers + (
-    'Access-Control-Allow-Origin',
-)
+# from corsheaders.defaults import default_headers
+# CORS_ALLOW_HEADERS = default_headers + (
+#     'Access-Control-Allow-Origin',
+# )
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
