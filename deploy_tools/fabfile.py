929f29bf5e40d77ab4addc7678f86e91ddd84cf4
scottessner <scott.essner@gmail.com>
2017-03-11 13:48:51 -0600

    added gunicorn and stopped collecting static

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -11,7 +11,7 @@ def deploy():
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
-    _update_static_files(source_folder)
+    #_update_static_files(source_folder)
     _update_database(source_folder)
 
 def _create_directory_structure_if_necessary(site_folder):
