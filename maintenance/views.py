6757c79b0ccca6d883efaef79f387d111f0a1cde
scottessner <scott.essner@gmail.com>
2017-05-09 22:30:38 -0500

    Updated database settings

--- maintenance/views.py
+++ maintenance/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from maintenance.models import Car, FillUp
-from maintenance.serializers import CarSerializer, FillupSerializer
+from maintenance.models import Car, FillUp, Repair
+from maintenance.serializers import CarSerializer, FillupSerializer, RepairSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -18,9 +18,16 @@ class FillUpViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('car',)
 
+class RepairViewSet(viewsets.ModelViewSet):
+    queryset = Repair.objects.all()
+    serializer_class = RepairSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('car',)
+
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'cars': reverse('car-list', request=request, format=format),
-        'fill-ups': reverse('fill-up-list', request=request, format=format)
+        'fill-ups': reverse('fill-up-list', request=request, format=format),
+        'repairs': reverse('repair-list', request=request, format=format)
     })
